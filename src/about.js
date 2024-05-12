461565298662a96f1aeffdcb937891deddfb6214
Donquellso <kamil080801@gmail.com>
2024-05-12 20:36:33 +0200

    About site

--- src/about.js
+++ src/about.js
@@ -1,14 +1,27 @@
 let about = document.createElement('div');
-about.innerHTML=`Witaj w restauracji "Smaki Świata"! Jesteśmy miejscem, gdzie pasja do kulinariów łączy się z miłością do podróży. Nasza restauracja powstała z myślą o tych, którzy pragną odkrywać nowe smaki i doznania kulinarne.
-
-W naszej kuchni kultywujemy różnorodność kulinarną, inspirując się tradycyjnymi recepturami z różnych zakątków świata oraz nowoczesnymi trendami kulinarystycznymi. Nasz zespół doświadczonych kucharzy stale eksperymentuje z nowymi smakami i świeżymi składnikami, aby dostarczyć Ci niezapomniane doznania smakowe.
-
-Dbamy o to, aby każdy posiłek był nie tylko pyszny, ale również estetycznie podany. Wierzymy, że jedzenie to nie tylko kwestia smaku, ale także wizualnego doznania, dlatego każde danie przygotowujemy z największą starannością, dbając o jego prezentację.
-
-W naszej restauracji stawiamy również na przyjazną atmosferę i doskonałą obsługę. Nasz personel jest zawsze gotowy, aby uczynić Twój pobyt u nas niezapomnianym. Niezależnie od tego, czy odwiedzasz nas na romantyczną kolację, rodzinny obiad czy spotkanie biznesowe, zawsze zadbamy o Twój komfort i zadowolenie.
-
-Dodatkowo, nasza restauracja oferuje również możliwość organizacji prywatnych imprez oraz cateringu na różnego rodzaju wydarzenia. Jesteśmy elastyczni i dostosowujemy się do indywidualnych potrzeb i preferencji naszych gości.
-
-Dołącz do naszej kulinarnej podróży i odkryj niezliczone smaki świata w jednym miejscu. Zapraszamy do restauracji "Smaki Świata" – gdzie każdy kęs to podróż pełna smakowych przygód!`;
+about.classList.add('about');
+let div1 = document.createElement('div');
+let div2 = document.createElement('div');
+let div3 = document.createElement('div');
+let div4 = document.createElement('div');
+const tabh2 = [];
+const tabp = [];
+let tab = [div1, div2, div3, div4];
+tab.forEach((div) => {
+    div.classList.add('aboutdiv');
+    about.appendChild(div);
+    let h2 = document.createElement('h2');
+    tabh2.push(h2);
+    div.appendChild(h2);
+    let p = document.createElement('p');
+    tabp.push(p);
+    div.appendChild(p);
+});
+tabh2[0].textContent = `Smaki świata`;
+tabp[0].textContent = `Jesteśmy miejscem, gdzie pasja do kulinariów łączy się z miłością do podróży. Nasza restauracja to miejsce, w którym każdy znajdzie coś dla siebie. Nasze dania to połączenie smaków z różnych zakątków świata, które zaskoczą nawet najbardziej wymagających smakoszy.`;
+tabh2[1].textContent = `Kulinarne Podróże: Smakowite Ekspedycje w Świecie Kuchni`;
+tabp[1].textContent = `W naszej restauracji nie ma stałego menu, ale coś zaskakującego czeka na Ciebie za każdym razem! Przygotowujemy dania inspirowane kuchniami z całego świata, podróżując od wschodnich smaków Azji po serca Medyny. Doświadcz różnorodności kulinarnych doznań w naszym nieustannie zmieniającym się menu.`;
+tabh2[2].textContent = `Sezonowe Skarby: Świeże Inspiracje z Lokalnych Źródeł`;
+tabp[2].textContent = `U nas nie ma miejsca na nudę! Nasza kuchnia opiera się na sezonowych produktach lokalnych dostawców, co oznacza, że każda wizyta to nowe odkrycie smaków. Pozwól nam zainspirować Cię świeżością i autentycznością naszych sezonowych skarbów.`;
 
 export default about;
